0f78fc92172197b727815b9569a8ff0b5fbf7e67
sina Parekari <sinaparekari@gmail.com>
2024-07-02 17:51:00 -0700

    four commit

--- src/Car.java
+++ src/Car.java
@@ -1,12 +1,9 @@
-public class Car extends Vehiche implements Cloneable,Comparable<Vehiche>{
+public class Car extends Vehiche implements Cloneable{
 
     @Override
     public void howMove() {
 
     }
 
-    @Override
-    public int compareTo(Vehiche o) {
-        return 0;
-    }
+
 }
